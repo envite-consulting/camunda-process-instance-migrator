6c7add53f2c9be1f3e7fd92a8fc2c63ba54248e1
CRoberto1926 <3205022+CRoberto1926@users.noreply.github.com>
2025-09-12 10:11:00 +0200

    envite fork initial setup

---
+++ src/main/java/de/envite/bpm/camunda/migrator/plan/LoadNewestDeployedVersion.java
@@ -0,0 +1,15 @@
+package de.envite.bpm.camunda.migrator.plan;
+
+import java.util.Optional;
+
+public interface LoadNewestDeployedVersion {
+
+	/**
+	 * Loads the newest DefinitionId for a process with given definitionKey
+	 * 
+	 * @param processDefinitionKey the definitionKey of the process
+	 * @return the {@link VersionedDefinitionId} of the process, or empty if it
+	 *         could not be found.
+	 */
+	Optional<VersionedDefinitionId> forProcessDefinitionKey(String processDefinitionKey);
+}
