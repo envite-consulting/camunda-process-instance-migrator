2d2e19255ea7fb0527496f4b3d0526f23bf43b34
Kálmán Képes <2853992+nyuuyn@users.noreply.github.com>
2026-02-27 09:22:00 +0100

    spotless

--- src/test/java/de/envite/bpm/camunda/migrator/integration/ProcessInstanceMigratorTest_CallActivity_Minor.java
+++ src/test/java/de/envite/bpm/camunda/migrator/integration/ProcessInstanceMigratorTest_CallActivity_Minor.java
@@ -8,8 +8,8 @@ import static de.envite.bpm.camunda.migrator.integration.TestHelper.startProcess
 import static de.envite.bpm.camunda.migrator.integration.assertions.ProcessInstanceListAsserter.assertThat;
 import static de.envite.bpm.camunda.migrator.integration.assertions.TaskListAsserter.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processEngine;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processEngine;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.repositoryService;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
